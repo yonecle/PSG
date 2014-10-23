5c0fc7dcd540dd48bb9bda00403ee63c39236f19
yonecle <yonecle2525@icloud.com>
2014-10-23 10:19:55 +0900

    update RegisterSetter

--- RegisterSetter.h
+++ RegisterSetter.h
@@ -6,7 +6,6 @@ public:
   virtual void set(byte data) = 0;
 };
 
-
 class ShiftRegisterSetter : public RegisterSetter {
 public:
   ShiftRegisterSetter(byte data, byte latch, byte clock)
@@ -14,9 +13,11 @@ public:
   {
   }
 
-  void set(byte data){
+//  virtual void set(byte data) = 0;
+
+  void set(byte data, int order){
     digitalWrite(latch_pin, LOW);
-    shiftOut(data_pin, clock_pin, MSBFIRST, data);
+    shiftOut(data_pin, clock_pin, order, data);
     digitalWrite(latch_pin, HIGH);
   }
 
@@ -24,6 +25,30 @@ private:
   byte data_pin;
   byte latch_pin;
   byte clock_pin;
+};
+
+class ShiftMSBRegisterSetter : public ShiftRegisterSetter {
+public:
+  ShiftMSBRegisterSetter(byte data, byte latch, byte clock)
+  : ShiftRegisterSetter(data, latch, clock)
+  {
+  }
+
+  void set(byte data){
+    ShiftRegisterSetter::set(data, MSBFIRST);
+  }
+};
+
+class ShiftLSBRegisterSetter : public ShiftRegisterSetter {
+public:
+  ShiftLSBRegisterSetter(byte data, byte latch, byte clock)
+  : ShiftRegisterSetter(data, latch, clock)
+  {
+  }
+
+  void set(byte data){
+    ShiftRegisterSetter::set(data, LSBFIRST);
+  }
 };
 
 #endif
